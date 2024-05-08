79d36d8a38748f78208c4716dbdd811b38e672d9
Britnell <t.a.britnell@gmail.com>
2024-05-08 14:43:49 +0200

    obsidian cron sync - 2024-05-08 14:43:48

---
+++ readme.md
@@ -0,0 +1,35 @@
+
+# basic obsidian sync
+
+## what
+a REALLY simple setup for syncing your obsidian
+if youre a dev and have git setup, feels nice to sync your notes into a PRIVATE repo
+
+## why
+i tried the [obsidian git community plugin](https://github.com/denolehov/obsidian-git) but it wasn't working for me.
+also that uses git.js or so, and apparently cant use ssh auth, which you should be doing.
+so i thought its simple enough for a bash script to commit & push regularly
+
+
+## instructions
+setup
+1. setup git cli w ssh keys
+2. download repo & open folder as vault in obsidian
+3. test
+4. setup cron job or other
+
+## 3. test 
+test run script in terminal
+will check for changes & commit
+every 3rd run it will push
+`./sync.sh master 3`
+( see output or ask [ai](https://huggingface.co/chat/) if it's not working )
+
+## 4. cron
+`crontab -e` to setup cron job
+add cron command as you wish
+
+```
+# cron job that runs every 6 minutes, pushing every 3rd run & logs to file
+*/6 * * * * ~/obsidiary/sync.sh master 3 log
+```
---
+++ sync.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+log_file="sync.log"
+
+# args
+
+if [ "$#" -lt 2 ]; then
+    echo "Usage: sync.sh <branch_name> <modulo_push_every_x_commits> <optional_'log'_flag> "
+    exit 1
+fi
+
+script_dir=$(dirname "$0")
+branch_name="$1"
+modulo="$2"
+log_flag="$3"
+
+# functions
+
+setup(){
+    cd "$script_dir"
+    current_datetime=$(date +"%Y-%m-%d %H:%M:%S")
+    git_commit_msg="obsidian cron sync - $current_datetime"
+
+    if [[ "$log_flag" == "log"  ]];then 
+        exec >> ./$log_file 2>&1
+    fi
+    echo -e "\n [$current_datetime] - running $branch_name $modulo $log_flag "
+    
+    if ! [[ "$modulo" -gt 0 ]]; then
+        echo " modulo must be positive int "
+        exit 1
+    fi
+}
+
+
+func_counter(){
+    count_file="counter"
+    if [ -f "$count_file" ]; then
+        RUN_COUNT=$(cat "$count_file")
+    else
+        RUN_COUNT=0
+    fi
+    RUN_COUNT=$((RUN_COUNT + 1))
+    echo "$RUN_COUNT" > "$count_file"
+    echo "$RUN_COUNT"
+}
+
+check_git(){
+    if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+        echo "Error: Current directory is not a Git repository."
+        exit 0
+    fi
+    git checkout "$branch_name" --quiet
+}
+
+commit_changes(){
+    if ! [ -z "$(git status --porcelain)" ]; then
+        echo "commit changes"
+        git add .
+        git commit -m "$git_commit_msg" --quiet
+    fi
+}
+
+push_on_modulo(){
+    count=$(func_counter)
+    if [ $((count % modulo)) -eq 0 ]; then
+        if [ $( git log --oneline @{u}..@  | wc -l ) -gt 0 ]; then 
+            echo "push to origin"
+            git push origin "$branch_name" --quiet
+        fi
+    fi
+}
+
+# main
+
+setup
+
+check_git
+
+commit_changes
+
+push_on_modulo
